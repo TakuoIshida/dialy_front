ab0f74bc9f37b9f9442799afa86e53be6c96d8d8
takuo_ishida <archersparadox.100@gmail.com>
2021-05-23 14:37:22 +0900

    不要なログを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import DialyList from '@/components/DialyList'
 import { initdialyList } from '@/dev/Actions'
-import { RootState } from '@/store'
 import { GetApiResponse, IProps } from '@/types/type'
 import { getDialy, judgeOwnSentiment } from '@/utils/functions'
 import { Button, Divider } from '@material-ui/core'
 import Link from 'next/link'
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 // eslint-disable-next-line no-restricted-imports
 import style from '../styles/_dialy_list.module.scss'
 // レンダリングするTopのコンポネントでAPIフェッチする必要がある
@@ -22,13 +21,6 @@ export async function getServerSideProps() {
 }
 
 const TopPage: React.FC<IProps> = (props: IProps) => {
-  console.log(props.dialyList)
-
-  // interface をかませる
-  const selectedDialy = useSelector((store: RootState) => store.reducksDaily)
-  console.log('selectedDialy')
-  console.log(selectedDialy)
-
   const dispatch = useDispatch()
   const handleDailyList = () => dispatch(initdialyList(props.dialyList))
   useEffect(() => {
